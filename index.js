dc75d7f3c0e85e0b858c29ca6962f5840d851e26
mcerisier <maxence.cerisier@etudiant.univ-rennes1.fr>
2022-03-09 23:29:12 +0100

    discordisation

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const fs = require("fs");
-const { parseString, Builder } = require("xml2js");
+const fs = require('fs');
+const {parseString, Builder} = require("xml2js");
 
-// Load the XML
-const xml = fs.readFileSync("PrixCarburants_instantane.xml").toString();
-parseString(xml, function (err, data) {
+const Discord = require('discord.js');
+const config = require('./config.json')
+const Client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
+});
 
-    var cp = 22300;
-    var carb = 'Gazole';
-    var i = 0;
-    // Show the XML
-    while(data.pdv_liste.pdv[i] != undefined){
-        if(data.pdv_liste.pdv[i].$.cp == cp){
-            console.log(data.pdv_liste.pdv[i].adresse);
-            var j = 0;
-            while(data.pdv_liste.pdv[i].prix[j] != undefined){
-                if(data.pdv_liste.pdv[i].prix[j].$.nom == carb){
-                    console.log(data.pdv_liste.pdv[i].prix[j].$.nom);
-                    console.log(data.pdv_liste.pdv[i].prix[j].$.valeur,'€');
-                    console.log('--------------------');
+Client.on('ready', () => {
+    console.log('The BOT is ready');
+});
+
+Client.on('messageCreate', message => {
+    if (message.content[0] === config.prefix) {
+        let splitMessage = message.content.split(" ");
+        if (splitMessage[0] === ".carbu") {
+            const xml = fs.readFileSync("PrixCarburants_instantane.xml").toString();
+            parseString(xml, function (err, data) {
+                var cp = splitMessage[1];
+                var carb = splitMessage[2];
+                var i = 0;
+                var reply = '';
+                // Show the XML
+                while (data.pdv_liste.pdv[i] != undefined) {
+                    if (data.pdv_liste.pdv[i].$.cp == cp) {
+                        var j = 0;
+                        while (data.pdv_liste.pdv[i].prix[j] != undefined) {
+                            if (data.pdv_liste.pdv[i].prix[j].$.nom == carb) {
+                                reply += data.pdv_liste.pdv[i].adresse+'\n';
+                                reply += data.pdv_liste.pdv[i].prix[j].$.nom+'\n';
+                                reply += data.pdv_liste.pdv[i].prix[j].$.valeur+'€'+'\n';
+                                reply += '--------------------\n';
+                            }
+                            j++;
+                        }
+                    }
+                    i++;
                 }
-                j++;
-            }
+                const embed = new Discord.MessageEmbed()
+                .setDescription(reply);
+                message.channel.send({ embeds: [embed]});
+            });
         }
-        i++;
     }
 });
+
+Client.login(config.token)
